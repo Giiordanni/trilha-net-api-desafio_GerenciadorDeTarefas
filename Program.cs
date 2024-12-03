8cc50a28d2881ae7939aa2d356e8a5bdaa366571
Giordanni <giordanniformiga103@gmail.com>
2024-12-03 12:42:20 -0300

    desafio finalizado

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@ using TrilhaApiDesafio.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+builder.Services.AddDbContext<OrganizadorContext>(options => 
+    options.UseMySql(builder.Configuration.GetConnectionString("ConexaoPadrao"),
+    new MySqlServerVersion(new Version(8, 0, 21))));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
